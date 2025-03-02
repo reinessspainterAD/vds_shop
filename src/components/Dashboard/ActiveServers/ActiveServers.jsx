3c2975ed2fcf47171c365ef06f90b7e5cc184b3b
Daniel A.G <reinesssspainter03@gmail.com>
2025-03-02 18:51:17 +0300

    Rework userCab(Dashboard)

--- src/components/Dashboard/ActiveServers/ActiveServers.jsx
+++ src/components/Dashboard/ActiveServers/ActiveServers.jsx
@@ -1,8 +1,16 @@
 import styles from './ActiveServers.module.css'
+import ServerBox from './ServerBox/ServerBox.jsx'
+
 function ActiveServers(){
+    //Заменить на запрос из бд
+    const data = [
+        {img: 'src/assets/ubuntu.svg', name: 'MineCraftServer', cpu: '11%', ram: '32%', hdd: '44%'},
+        {img: 'src/assets/debian.svg', name: 'MyWebsite', cpu: '50%', ram: '25%', hdd: '15%'},
+        {img: 'src/assets/winserver.svg', name: 'asfger_asd111', cpu: '45%', ram: '15%', hdd: '86%'}
+    ]
     return(
-        <div>
-            Активные серверы
+        <div className={styles.div}>
+            <ServerBox data={data}/>
         </div>
     )
 }
